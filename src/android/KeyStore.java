a0fe59b88942013cf13e5d893994c1fc91d1f4fe
Mark Artishuk <markyshuk@gmail.com>
2021-06-30 13:04:31 -0700

    implemented with sendPluginResult

--- src/android/KeyStore.java
+++ src/android/KeyStore.java
@@ -21,58 +21,38 @@ public class KeyStore extends CordovaPlugin {
 
 
     public KeyStore() {
-        // Context context = this.cordova.getActivity().getApplicationContext();
-        // sharedPreferences = context.getSharedPreferences("data", Context.MODE_PRIVATE );
-        // editor = sharedPreferences.edit();
+        Context context = this.cordova.getActivity().getApplicationContext();
+        sharedPreferences = context.getSharedPreferences("data", Context.MODE_PRIVATE );
+        editor = sharedPreferences.edit();
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "Check me out here");
-        callbackContext.sendPluginResult(pluginResult);
-        return true;
-
-        // if (action.equals("setItem")) {
-        //     String key = args.getString(0);
-        //     String val = args.getString(1);
-        //     this.setItem(key, val, callbackContext);
-        //     return true;
-        // }
-        // if(action.equals("getItem")) {
-        //     String key = args.getString(0);
-        //     this.getItem(key, callbackContext);
-        //     return true;
-        // }
-        // return false;
+        
+        if (action.equals("setItem")) {
+            String key = args.getString(0);
+            String val = args.getString(1);
+            this.setItem(key, val, callbackContext);
+            return true;
+        }
+        if(action.equals("getItem")) {
+            String key = args.getString(0);
+            this.getItem(key, callbackContext);
+            return true;
+        }
+        return false;
     }
 
-    // private void setItem(String key, String value, CallbackContext cb) {
-    //        if(sharedPreferences == null) {
-    //             cb.error("shared preferences is null");
-    //             return;
-    //         }
+    private void setItem(String key, String value, CallbackContext cb) {
+        editor.putString(key, value);
+        editor.commit();
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, "Set -> " + key + ":" + value);
+        cb.sendPluginResult(pluginResult);
+    }
 
-    //         if(editor == null) {
-    //             cb.error("Editor is null");
-    //             return;
-    //         }
-    //         editor.putString(key, value);
-    //         editor.commit();
-
-    //         cb.success("Hey!: " + key + " " + value);
-
-    // }
-
-    // private void getItem(String key, CallbackContext cb) {
-    //      String val = sharedPreferences.getString(key, "");
-    //      cb.success(val);
-    // }
-
-    // private void echo(String message, CallbackContext callbackContext) {
-    //     if (message != null && message.length() > 0) {
-    //         callbackContext.success(message);
-    //     } else {
-    //         callbackContext.error("Expected one non-empty string argument.");
-    //     }
-    // }
+    private void getItem(String key, CallbackContext cb) {
+        String val = sharedPreferences.getString(key, "");
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, val);
+        cb.sendPluginResult(pluginResult);
+    }
 }
